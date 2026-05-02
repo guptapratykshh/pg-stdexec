28d14af4a457d4acb6afd26a3e74a9b6f6929a11
Eric Niebler <eniebler@nvidia.com>
2026-05-02 13:12:41 -0700

    temporary relacy workaround until dvyukov/relacy#48 lands

--- test/rrd/stdexec_relacy.hpp
+++ test/rrd/stdexec_relacy.hpp
@@ -2,8 +2,16 @@
 
 #include "../../relacy/relacy_std.hpp"
 
+#include <iosfwd>
+#include <thread>
+
 namespace std
 {
   template <class>
   struct atomic_ref;
+
+  inline std::ostream& operator<<(std::ostream& os, std::thread::id const & id)
+  {
+    return os << "thread::id{" << id.id_ << "}";
+  }
 }  // namespace std
